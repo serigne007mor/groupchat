bf9169aefc87a60d66220ece6f854894cf0aef02
Serigne Toure <serignemortoure1@gmail.com>
2021-03-18 02:28:44 +0000

    added chat functionallity

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const http = require('http').createServer();
+
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+})
+io.on('connection', (socket) => {
+    console.log('user connected')
+    socket.on('message', (message => {
+        console.log(message)
+        io.emit(message, `${socket.id.substr(0,2)}: ${message}`)
+    }))
+})
+
+http.listen(8080, () => console.log('listening on http://localhost:8080'));
+
+// Regular Websockets
+
+// const WebSocket = require('ws')
+// const server = new WebSocket.Server({ port: '8080' })
+
+// server.on('connection', socket => { 
+
+//   socket.on('message', message => {
+
+//     socket.send(`Roger that! ${message}`);
+
+//   });
+
+// });
